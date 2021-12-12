08eb5722414a8783aeeb059c586d87a77934dcfd
grzegorzrychlin <rychlin@student.wszib.edu.pl>
2021-12-12 14:40:56 +0100

    Nowe pliki

---
+++ live_coding/20. Edycja zawartości elementu/script.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    document.getElementById('element1').textContent = 'Zawartość elementu';
+    document.getElementById('element2').innerHTML = '<b>Zawartość elementu 2</b>';
+
+
+};
