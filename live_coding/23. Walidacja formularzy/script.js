d435ffab692c5db4eb1c0c949dff87d7ac0adf0a
grzegorzrychlin <rychlin@student.wszib.edu.pl>
2021-12-12 16:39:38 +0100

    Nowe pliki

---
+++ live_coding/23. Walidacja formularzy/script.js
@@ -0,0 +1,37 @@
+window.onload = function () {
+    let username = document.getElementById('username');
+    let error = document.getElementById('error');
+    document.getElementById('form').addEventListener('submit', function (e) {
+        e.preventDefault();
+        if (validateUsername()) {
+            hideError();
+            window.alert('Formularz został wysłany')
+        }
+    });
+
+    function validateUsername() {
+        let userValid = true;
+        if (!checkRequired()) {
+            userValid = false;
+            showErrorText('Nazwa użytkownika nie może być pusta.');
+        } else if (!checkMinMaxLenght()) {
+            userValid = false;
+            showErrorText('Nazwa użytkownika powinna mieć długość od 3 do 10 znaków');
+        }
+        return userValid;
+    }
+    function checkRequired() {
+        return username.value.length > 0;
+    }
+    function checkMinMaxLenght() {
+        return username.value.length >= 3 && username.value.length <= 10;
+    }
+    function showErrorText(err) {
+        error.textContent = err;
+        error.removeAttribute('hidden');
+    }
+    function hideError() {
+        error.setAttribute('hidden', '');
+    }
+
+};
